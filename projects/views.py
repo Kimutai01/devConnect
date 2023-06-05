92c1db5df382ed2d29c6f90ae466727a331bd365
Kimutai01 <kiprotichkimutai01@gmail.com>
2023-06-05 13:59:37 +0300

    Created projects model

--- projects/views.py
+++ projects/views.py
@@ -1,10 +1,39 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+all_projects=[
+    {
+        'id':'1',
+        'title':'portfolio website',
+        'description':'cool stuff'
+
+    },
+    {
+        'id':'2',
+        'title':'Ecommerce website',
+        'description':'cool stuff'
+
+    },
+    {
+        'id':'3',
+        'title':'Social network',
+        'description':'cool stuff'
+
+    }
+]
+
 def projects(request):
-    return HttpResponse("Here are our products")
+    msg='Hello'
+    number=1
+
+    context={"msg":msg, "num":number, 'projects':all_projects}
+    return render(request,'projects/projects.html',context)
 
 def project(request, pk):
-    return HttpResponse("Single project " + " " + pk)
+    projectObj= None
+    for i in all_projects:
+        if i['id']==pk:
+            projectObj=i
+    return render(request, 'projects/single-project.html', {"project":projectObj})
 
 
