65cc18d9858db41d7662a7bf1ce415b5ff614d72
Kimutai01 <kiprotichkimutai01@gmail.com>
2023-06-05 16:15:40 +0300

    Added tags

--- projects/models.py
+++ projects/models.py
@@ -9,9 +9,32 @@ class Project(models.Model):
     description = models.TextField(null=True,blank=True)
     demo_link = models.CharField(max_length = 2000, blank=True, null=True)
     source_link = models.CharField(max_length = 2000, blank=True, null=True)
+    tags = models.ManyToManyField('Tag', blank=True)
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
 
     def __str__(self):
         return self.title
+
+
+class Review(models.Model):
+    
+    #owner
+    project= models.ForeignKey(Project,on_delete=models.CASCADE)
+    body = models.TextField(null=True,blank=True)
+    value = models.IntegerField()
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    
+    def __int__(self):
+        return self.value
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+       return self.name
