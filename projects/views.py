3c58ba3d7fe941d93dbba7d379b821e64f1a5a8a
Kimutai01 <kiprotichkimutai01@gmail.com>
2023-06-05 16:57:47 +0300

    Database query

--- projects/views.py
+++ projects/views.py
@@ -1,39 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Project
 
-all_projects=[
-    {
-        'id':'1',
-        'title':'portfolio website',
-        'description':'cool stuff'
 
-    },
-    {
-        'id':'2',
-        'title':'Ecommerce website',
-        'description':'cool stuff'
-
-    },
-    {
-        'id':'3',
-        'title':'Social network',
-        'description':'cool stuff'
-
-    }
-]
 
 def projects(request):
-    msg='Hello'
-    number=1
+    projects = Project.objects.all()
 
-    context={"msg":msg, "num":number, 'projects':all_projects}
+    context={'projects':projects}
     return render(request,'projects/projects.html',context)
 
 def project(request, pk):
-    projectObj= None
-    for i in all_projects:
-        if i['id']==pk:
-            projectObj=i
+    projectObj= Project.objects.get(id=pk)
+    
+    
     return render(request, 'projects/single-project.html', {"project":projectObj})
+
+
+def createProject(request):
+    context={}
+    return render(request, 'projects/project_form.html',context )
 
 
