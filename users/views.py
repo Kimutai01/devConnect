a935c2a8244aa01ccf56dd8ea81315fb6f6aa444
Kimutai01 <kiprotichkimutai01@gmail.com>
2023-06-13 06:06:07 +0300

    Added authentication

--- users/views.py
+++ users/views.py
@@ -4,11 +4,14 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth.models import User
 from .models import Profile
+from .forms import CustomUserCreationForm
 
 # Create your views here.
 
 
 def loginUser(request):
+    page='login'
+    context={'page':page}
     if request.user.is_authenticated:
         return redirect('profiles')
     if request.method=="POST":
@@ -28,12 +31,33 @@ def loginUser(request):
         else:
             messages.error(request,'username or password is incorrect')
            
-    return render(request, 'users/login_register.html')
+    return render(request, 'users/login_register.html',context)
 
 def logoutUser(request):
     logout(request)
     messages.error(request,'User was logged out!')
     return redirect('login')
+
+def registerUser(request):
+    page='register'
+    form=CustomUserCreationForm()
+    if request.method=='POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            
+            messages.success(request, 'User account was created')
+            
+            login(request,user)
+            
+            return redirect('profiles')
+        
+        else:
+            messages.error(request, 'An error has occured during registration')
+    context={'page':page,'form':form}
+    return render(request,'users/login_register.html', context)
 
 def profiles(request):
     profiles =Profile.objects.all
