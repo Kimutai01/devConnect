c015f19b29f09fdbfe44421ce3a602b00ea47cdc
Kimutai01 <kiprotichkimutai01@gmail.com>
2023-06-29 16:16:34 +0300

    Edit profile

--- users/views.py
+++ users/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth.models import User
 from .models import Profile
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, ProfileForm
 
 # Create your views here.
 
@@ -52,7 +52,7 @@ def registerUser(request):
             
             login(request,user)
             
-            return redirect('profiles')
+            return redirect('edit-account')
         
         else:
             messages.error(request, 'An error has occured during registration')
@@ -80,3 +80,18 @@ def userAccount(request):
     print(topSkills)
     context={'profile':profile, 'top':topSkills, 'projects':projects}
     return render(request, 'users/account.html', context)
+
+
+@login_required(login_url='login')
+def editAccount(request):
+    profile = request.user.profile
+    form = ProfileForm(instance=profile)
+    
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid:
+            form.save()
+            
+            return redirect('account')
+    context={'form': form}
+    return render(request, 'users/profile_form.html', context)
