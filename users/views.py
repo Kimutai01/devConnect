f66279f12e66126a1b5f504b4b3d1cfc4356b59b
Kimutai01 <kiprotichkimutai01@gmail.com>
2023-07-04 10:24:22 +0300

    Adding pagination for profiles page

--- users/views.py
+++ users/views.py
@@ -6,7 +6,7 @@ from django.contrib import messages
 from django.contrib.auth.models import User
 from .models import Profile
 from .forms import CustomUserCreationForm, ProfileForm, SkillForm
-from .utils import searchProfiles
+from .utils import searchProfiles,paginateProfiles
 
 
 # Create your views here.
@@ -64,8 +64,9 @@ def registerUser(request):
 
 def profiles(request):
     profiles, search_query = searchProfiles(request)
+    custom_range, profiles = paginateProfiles(request, profiles, 3)
    
-    context={'profiles':profiles, 'search_query':search_query}
+    context={'profiles':profiles, 'search_query':search_query,'custom_range':custom_range}
     return render(request, 'users/profiles.html',context)
 
 def userProfile(request,pk):
